fc61f51c9f7434a54be0b63fc4bd092bcc78f75f
gafur55 <g_mammadov@u.pacific.edu>
2023-03-28 18:17:43 -0700

    linter

--- movie_list.py
+++ movie_list.py
@@ -1,22 +1,52 @@
-'''
-This class is for the movieLists object
-'''
+"""
+This module defines a MovieList class that represents a list of watched and unwatched movies.
+"""
 
 from movie import Movie
 
 class MovieList:
+    """
+    Represents a list of watched and unwatched movies.
+
+    Attributes:
+        not_watched (list): A list of movies that have not been watched.
+        watched (list): A list of movies that have been watched.
+    """
+
     def __init__(self, not_watched, watched):
+        """
+        Initializes a new instance of the MovieList class.
+
+        Args:
+            not_watched (list): A list of movies that have not been watched.
+            watched (list): A list of movies that have been watched.
+        """
         self.not_watched = not_watched
         self.watched = watched
 
     def add_new_movie(self, movie):
+        """
+        Adds a new movie to the list of unwatched movies.
+
+        Args:
+            movie (Movie): The movie to add to the list.
+        """
         self.not_watched.append(movie)
 
     def already_watched(self, movie):
+        """
+        Moves a movie from the list of unwatched movies to the list of watched movies.
+
+        Args:
+            movie (Movie): The movie to move to the list of watched movies.
+        """
         self.not_watched.remove(movie)
         self.watched.append(movie)
 
     def display_all(self):
+        """
+        Displays all movies in the lists of watched and unwatched movies.
+        """
         print("watched list")
         for i in self.not_watched:
             print(i.get_title())
@@ -25,6 +55,9 @@ class MovieList:
             print(i.get_title())
 
 def main():
+    """
+    The main function of the program.
+    """
     arr1 = []
     arr2 = []
 
@@ -32,18 +65,16 @@ def main():
     movie2= Movie("Titanic2", 2)
     movie3 = Movie("Titanic3", 2)
 
-    movieList1 = MovieList(arr1, arr2)
+    movie_list1 = MovieList(arr1, arr2)
 
-    movieList1.add_new_movie(movie1)
-    movieList1.add_new_movie(movie2)
-    movieList1.add_new_movie(movie3)
+    movie_list1.add_new_movie(movie1)
+    movie_list1.add_new_movie(movie2)
+    movie_list1.add_new_movie(movie3)
 
-    # movieList1.displayAll()
-    movieList1.already_watched(movie1)
+    # movie_list1.displayAll()
+    movie_list1.already_watched(movie1)
 
-    movieList1.display_all()
-
-
+    movie_list1.display_all()
 
 if __name__ == "__main__":
     main()
