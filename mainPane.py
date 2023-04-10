6e4801b9b9851ff5edc8e1095c723fd0751ea87e
gafur55 <g_mammadov@u.pacific.edu>
2023-04-09 23:02:12 -0700

    user input added. Is fully functional

--- mainPane.py
+++ mainPane.py
@@ -1,6 +1,7 @@
 from graphics import *
 from movie_list import MovieList
 from movie import Movie
+import tkinter as tk
 
 # public movie list
 global movies_list
@@ -18,8 +19,14 @@ SPACING_Y = 50
 
 def run_app():
     # create a window
-    win = GraphWin("Movie List", 1050, 600)
+    # root1 = tk.Tk()
+    win = GraphWin("Movie List", 1050, 600, autoflush=False)
 
+     #create add movie button
+    add_movie_button = Rectangle(Point(900, 10), Point(1000, 35))
+    add_movie_button_text = Text(Point(950, 22), "Add Movie")
+    add_movie_button.draw(win)
+    add_movie_button_text.draw(win)
     # create a list of movies
     global movies_list
 
@@ -41,7 +48,7 @@ def run_app():
         title.setSize(12)
         title.setTextColor("white")
         title.draw(win)
-
+       
         # create star polygon
         rating = int(movie.get_rating())
         for j in range(rating):
@@ -66,15 +73,28 @@ def run_app():
     # wait for user input
     win.getMouse()
 
+    while True:
+        click = win.getMouse()
+        if (click.getX() >= 900 and click.getX() <= 1000 and click.getY() >= 10 and click.getY() <= 35):
+            win.delete("all")
+            win.close()
+            create_input_page()
+            
+
+        elif click == None:
+            break
+
+        
     # close the window
     win.close()
+    # root1.destroy()
 
 
 def create_movie_list():
     global movies_list
     # movie_data_list = [] # getting all movie data
 
-    with open('movieData.txt', 'r') as file:
+    with open('movieData.txt', 'r+') as file:
         lines = [line.strip() for line in file.readlines()]
 
     for line in lines:
@@ -83,6 +103,73 @@ def create_movie_list():
         movie = Movie(data[0], data[1], data[2],  data[3])
         movies_list.append(movie)
         # movie_data_list.append(data)
+    file.close()
+
+def create_input_page():
+
+    win2 = GraphWin("User Inputs", 850, 600, autoflush=False)
+
+    # create an Entry widget for user input
+    title_input_text = Text(Point(200, 70), "Enter Movie Title")
+    title_input = Entry(Point(200, 100), 30)
+    title_input.setFill("white")
+    title_input.draw(win2)
+    title_input_text.draw(win2)
+
+    star_input_text = Text(Point(200, 170), "Enter Movie Star (1-5)")
+    star_input = Entry(Point(200, 200), 30)
+    star_input.setFill("white")
+    star_input.draw(win2)
+    star_input_text.draw(win2)
+
+    comment_input_text = Text(Point(600, 70), "Enter Your Comments")
+    comment_input = Entry(Point(600, 100), 30)
+    comment_input.setFill("white")
+    comment_input.draw(win2)
+    comment_input_text.draw(win2)
+
+    path_input_text = Text(Point(600, 170), "Enter Path to the Image")
+    path_input = Entry(Point(600, 200), 30)
+    path_input.setFill("white")
+    path_input.draw(win2)
+    path_input_text.draw(win2)
+
+    # create a Text variable to save inputs
+    saved_text = ''
+
+    # create a button to save input to Text widget
+    save_button = Rectangle(Point(350, 300), Point(450, 325))
+    save_button_text = Text(Point(400, 310), "Save Input")
+    save_button.draw(win2)
+    save_button_text.draw(win2)
+
+    # main loop to wait for user input and handle button clicks
+    while True:
+        click = win2.getMouse()
+        
+        # check if save button was clicked
+        if (click.getX() >= 350 and click.getX() <= 450
+            and click.getY() >= 300 and click.getY() <= 325):
+            
+            # save input to Text widget
+            saved_text = title_input.getText() + ',' + star_input.getText() + ',' + comment_input.getText() +',' + path_input.getText()+'\n'
+            print(saved_text)
+            with open('movieData.txt', 'a') as file:
+                file.write(saved_text)
+            file.close()
+            global movies_list
+            movies_list.clear()
+            create_movie_list()
+            win2.close()
+            run_app()
+            
+            # win2.close()
+        # check if window was closed
+        elif click == None:
+            break
+
+
+    win2.close()
 
 
 
@@ -95,3 +182,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# spiderman,4,liked,Movie_Images/The_Last_Jedi_resized.png
+# spider man,3,asdasdasda,Movie_Images/Spider Man_resized.png
