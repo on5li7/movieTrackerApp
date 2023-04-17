aadda15401d23f5511ac397544e36f59decde12d
CalvinZhuang <c_zhuang@u.pacific.edu>
2023-04-16 21:47:09 -0700

    added testmovieimput function

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import unittest
 from mainPane import *
+from movie import Movie
 
 class TestMovieList(unittest.TestCase):
 
@@ -10,6 +11,11 @@ class TestMovieList(unittest.TestCase):
         self.assertEqual(movies_list[0].get_rating(), "4")
         self.assertEqual(movies_list[0].get_comment(), "I liked it")
         self.assertEqual(movies_list[0].get_image_path(), "Movie_Images/The_Last_Jedi_resized.png")
+
+class TestMovieInput(unittest.TestCase):
+    def test_saved_moive_rating(self):
+        create_movie_list()
+        self.assertTrue(1 <= int(movies_list[0].get_rating()) <= 5)
 
 if __name__ == '__main__':
     unittest.main()
