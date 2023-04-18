0ea93e9db1707e4683367ac16d20cd20c4459f7b
Ryan Phan <r_nguyen18@u.pacific.edu>
2023-04-17 22:24:42 -0700

    created list for function currently_watching

--- currently_watching.py
+++ currently_watching.py
@@ -35,4 +35,27 @@ def run_app():
     back_to_menu_button.draw(win)
     back_to_menu_button_text.draw(win)
 
+    # create a list of movies
+    global movies_list
+    global movie_coords
+
+    movie_objects = []
+    rectangles = []
+    for i, movie in enumerate(movies_list):
+        x = MARGIN_X + (i % 5) * (RECTANGLE_WIDTH + SPACING_X)
+        y = MARGIN_Y + (i // 5) * (RECTANGLE_HEIGHT + SPACING_Y)
+
+        img = Image(Point(x + RECTANGLE_WIDTH/2, y + RECTANGLE_HEIGHT/2), movie.get_image_path())
+        img.draw(win)
+
+        rect = Rectangle(Point(x, y), Point(x + RECTANGLE_WIDTH, y + RECTANGLE_HEIGHT))
+        movie_coords.append([x, x + RECTANGLE_WIDTH, y, y + RECTANGLE_HEIGHT])
+        #rect.setFill(color_rgb(200, 200, 200))
+        rect.draw(win)
+        rectangles.append(rect)
+        # create movie title
+        title = Text(Point(x + RECTANGLE_WIDTH / 2, y + RECTANGLE_HEIGHT + 15), f"{i+1}. {movie.get_title()}")
+        title.setSize(12)
+        title.setTextColor("white")
+        title.draw(win)
        
