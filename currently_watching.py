83d4eaa1abb47e095b2f7b62953e18d4a470d98a
Ryan Phan <r_nguyen18@u.pacific.edu>
2023-04-17 22:57:42 -0700

    Add data text for currently watching list Almost completed currently_watcheing.py

--- currently_watching.py
+++ currently_watching.py
@@ -20,7 +20,7 @@ SPACING_Y = 70
 def run_app():
     # create a window
     # root1 = tk.Tk()
-    win = GraphWin("NOT WATCHED Movie List", 1050, 600, autoflush=False)
+    win = GraphWin("Currently Wathcing Movie List", 1050, 600, autoflush=False)
     win.setBackground("dark grey")
 
      #create add movie button
@@ -77,8 +77,153 @@ def run_app():
             #star.setFill("yellow")
             #star.draw(win)
 
-        # store both objects in a list
+        #store both objects in a list
         #movie_objects.append((rect, star, Text))
         movie_objects.append((rect,Text))
 
+#print("Coordinates of the movie rectangles", movie_coords)
+# wait for user input
+#win.getMouse()
+
+#while True:
+    click = win.getMouse()
+    if (click.getX() >= 900 and click.getX() <= 1000 and click.getY() >= 10 and click.getY() <= 35):
+        win.delete("all")
+        win.close()
+        create_input_page()
+    for i in range(len(movie_coords)):
+        if click.getX() >= movie_coords[i][0] and click.getX() <= movie_coords[i][1] and click.getY() >= movie_coords[i][2] and click.getY() <= movie_coords[i][3]:
+            show_comments(i)  # Close the window
+    if (click.getX() >= 750 and click.getX() <= 850 and click.getY() >= 10 and click.getY() <= 35):
+        win.close()
+        movie_coords.clear()
+        movies_list.clear()
+        menu_page.main()
+       
+       
+       
+       
+        # elif click == None:
+        #     break
+
+        
+    # close the window
+    win.close()
+    # root1.destroy()
+
+
+def create_movie_list():
+    global movies_list
+    # movie_data_list = [] # getting all movie data
+
+    with open('currentlyWatchingMovieData.txt', 'r+') as file:
+        lines = [line.strip() for line in file.readlines()]
+
+    for line in lines:
+        data = line.split(",")
+        print(data)
+        movie = Movie(data[0], data[1], data[2],  data[3])
+        movies_list.append(movie)
+        # movie_data_list.append(data)
+    file.close()
+
+def create_input_page():
+
+    win2 = GraphWin("User Inputs", 850, 600, autoflush=False)
+
+    # create an Entry widget for user input
+    title_input_text = Text(Point(200, 70), "Enter Movie Title")
+    title_input = Entry(Point(200, 100), 30)
+    title_input.setFill("white")
+    title_input.draw(win2)
+    title_input_text.draw(win2)
+
+    star_input_text = Text(Point(200, 170), "Enter Movie Star (1-5)")
+    star_input = Entry(Point(200, 200), 30)
+    star_input.setFill("white")
+    star_input.draw(win2)
+    star_input_text.draw(win2)
+
+    comment_input_text = Text(Point(600, 70), "Enter Your Comments")
+    comment_input = Entry(Point(600, 100), 30)
+    comment_input.setFill("white")
+    comment_input.draw(win2)
+    comment_input_text.draw(win2)
+
+    path_input_text = Text(Point(600, 170), "Enter Path to the Image")
+    path_input = Entry(Point(600, 200), 30)
+    path_input.setFill("white")
+    path_input.draw(win2)
+    path_input_text.draw(win2)
+
+    # create a Text variable to save inputs
+    saved_text = ''
+
+    # create a button to save input to Text widget
+    save_button = Rectangle(Point(350, 300), Point(450, 325))
+    save_button_text = Text(Point(400, 310), "Save Input")
+    save_button.draw(win2)
+    save_button_text.draw(win2)
+
+    #create a back button 
+    back_button = Rectangle(Point(350, 400), Point(450, 425))
+    back_button_text = Text(Point(400, 410), "Back")
+    back_button.draw(win2)
+    back_button_text.draw(win2)
+
+    # main loop to wait for user input and handle button clicks
+    while True:
+        click = win2.getMouse()
+        
+        # check if save button was clicked
+        if (click.getX() >= 350 and click.getX() <= 450
+            and click.getY() >= 300 and click.getY() <= 325):
+            
+            # save input to Text widget
+            saved_text = title_input.getText() + ',' + star_input.getText() + ',' + comment_input.getText() +',' + path_input.getText()+'\n'
+            print(saved_text)
+            with open('currentlyWatchingData.txt', 'a') as file:
+                file.write(saved_text)
+            file.close()
+            win2.close()
+            # refresh_main_page()
+
+        if (click.getX() >= 350 and click.getX() <= 450 
+            and click.getY() >= 400 and click.getY() <= 425):
+            win2.close()
+            refresh_main_page()
+            
+            # win2.close()
+        # check if window was closed
+        elif click == None:
+            break
+
+
+    win2.close()
+
+#rerun the main page and update global variables
+def refresh_main_page():
+    global movies_list
+    movies_list.clear()
+    movie_coords.clear()
+    create_movie_list()
+    run_app()
+
+def show_comments(i):
+    win_comment = GraphWin("Comment Window", 1000, 200)
+    comment = Text(Point(700, 50), movies_list[i].get_comment())
+    comment.draw(win_comment)
+    time.sleep(2)
+
+    win_comment.close()
+
+
+def main():
+    create_movie_list()
+    run_app()
     
+    
+
+
+if __name__ == '__main__':
+    main()    
