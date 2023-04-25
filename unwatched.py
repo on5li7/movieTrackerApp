96e881b802f2cdf8e3ea2a09785c2b6da7b8035b
Sophie Tran <s_tran39@u.pacific.edu>
2023-04-25 14:38:16 -0700

    buttons bg color and currently watching bg change

--- unwatched.py
+++ unwatched.py
@@ -29,12 +29,14 @@ def run_app():
 
      #create add movie button
     add_movie_button = Rectangle(Point(900, 10), Point(1000, 35))
+    add_movie_button.setFill("light gray")
     add_movie_button_text = Text(Point(950, 22), "Add Movie")
     add_movie_button.draw(win)
     add_movie_button_text.draw(win)
 
     #create a back to menu button
     back_to_menu_button = Rectangle(Point(750, 10), Point(850, 35))
+    back_to_menu_button.setFill("light gray")
     back_to_menu_button_text = Text(Point(800, 22), "Menu")
     back_to_menu_button.draw(win)
     back_to_menu_button_text.draw(win)
